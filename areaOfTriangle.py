c8eb2658e3c7b2dc587653654c4cbb8f16ee8224
Abhi0412Shek <115910967+Abhi0412Shek@users.noreply.github.com>
2022-10-16 09:18:30 +0530

    area

---
+++ areaOfTriangle.py
@@ -0,0 +1,12 @@
+a = float(input("Enter first side of triangle"))
+b = float(input("Enter second side of triangle"))
+c = float(input("Enter third side of triamgle"))
+
+
+
+s = (a +b + c)/2
+
+
+
+area = s*(s-a)*(s-b)*(s-c)**0.5
+print= area
