dad6d54999e71eb78b60c11f814389751e3a0d2b
Abhi0412Shek <115910967+Abhi0412Shek@users.noreply.github.com>
2022-10-18 23:46:02 +0530

    oddeven

---
+++ oddEven.py
@@ -0,0 +1,10 @@
+#take user input (int)
+#print if the given number is odd or even
+num = int(input("enter a number:"))
+if (num % 2) == 0:
+    print("{0 is even}".format(num))
+else:
+    print("{0}is odd".format(num))
+
+
+
